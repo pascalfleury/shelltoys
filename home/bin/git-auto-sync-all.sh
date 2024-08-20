678be67bc0242625585beabe37746bf0c558ab22
Pascal Fleury <fleury@google.com>
2024-08-20 14:55:31 +0200

    D home/bin/#git-auto-sync-all.sh#  D home/bin/.#git-auto-sync-all.sh  M home/bin/git-auto-sync-all.sh

--- home/bin/git-auto-sync-all.sh
+++ home/bin/git-auto-sync-all.sh
@@ -1,15 +1,55 @@
 #!/bin/bash
 
-GIT_AUTO_SYNC="$(which git-auto-sync)"
-if [[ -z "${GIT_AUTO_SYNC}" ]]; then
-    echo "No git-auto-sync binary found !" >&2
-    exit 1
-fi
+readonly SUCCESS=0
+readonly FAILURE=1
+
+function LOG() {
+    local level="$1"
+    shift 1
+    case "${level}" in
+        WARN*)  echo "[${level}] $*" >&2 ;;
+        INFO)   echo "[${level}] $*" >&2 ;;
+        FATAL)  echo "[${level}] $*" >&2 ; kill $$ ;;
+        *)      echo "[DEBUG-${level}] $*" >&2 ;;
+    esac
+}
+
+function is_tty() {
+    [ -t 1 ] && return ${SUCCESS}
+    return ${FAILURE}
+}
+
+function find_exec() {
+    local path="$1"
+    [[ -z "${path}" ]] && LOG FATAL "find_exec needs a non-empty path"
+    local exec="$(which "${path}")"
+    [[ -z "${exec}" ]] && LOG FATAL "Could not find binary '${path}'"
+    echo ${exec}
+}
+
+function needs_gcert() {
+    local remote="${1:-origin}"
+    find_exec git >/dev/null
+    case "$(git remote get-url ${remote})" in
+        sso://*)
+            find_exec gcertstatus >/dev/null
+            gcertstatus --quiet && return ${FAILURE}
+            return ${SUCCESS}
+            ;;
+        *) return ${FAILURE}
+           ;;
+    esac
+}
+
+GIT_AUTO_SYNC="$(find_exec git-auto-sync)"
 
 for repos in $(${GIT_AUTO_SYNC} daemon list); do
     echo "Syncing repository ${repos} ..." >&2
     (
         cd "${repos}"
-        ${GIT_AUTO_SYNC} sync
+        if is_tty && needs_gcert; then
+            gcert || LOG FATAL "Could not run gcert!"
+        fi
+        gcert && ${GIT_AUTO_SYNC} sync
     )
 done
